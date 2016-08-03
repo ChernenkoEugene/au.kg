ed6bd778514c19db76eba49dd717215ca3555f70
Eugene Chernenko <chernenko.ievg@gmail.com>
2016-08-04 01:14:33 +0300

    4

--- js/common.js
+++ js/common.js
@@ -1,26 +1,150 @@
 $(document).ready(function() {
-
+    //choose_ccountry
     $('.head_block_1 .cur_country').on('click', function() {
         $(this).toggleClass('open');
         $('.choose_country_list').slideToggle(200).toggleClass('hidden');
     });
 
+    //mobile_menus
+    if ($(window).width() < 1040) {
+        $(".categ_list li a").html("");
+    }
+    $(window).resize(function() {
+        if ($(window).width() < 1040) {
+            $(".categ_list li a").html("");
+        }
+    });
 
 
-(function($) {
-$(function() {
+    //mobile_search_form
+    if ($(window).width() < 1040) {
+        $(".main_block_search_form label:nth-child(3)").insertBefore("#m_search_how_much");
+    }
 
-	$('ul.tabs__caption').on('click', 'li:not(.active)', function() {
-		$(this)
-			.addClass('active').siblings().removeClass('active')
-			.closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
-	});
+    $(window).resize(function() {
+        if ($(window).width() < 1040) {
+            $(".label_howm").insertBefore("#m_search_how_much");
+        }
+    });
+    if ($(window).width() < 480) {
+        $(".main_block_search_form .label_where").insertBefore("#m_search_where");
+    }
 
-});
-})(jQuery);
+    $(window).resize(function() {
+        if ($(window).width() < 480) {
+            $(".main_block_search_form .label_where").insertBefore("#m_search_where");
+        }
+    });
+    //mobile_review
+    if ($(window).width() < 900) {
+        $('.review .review_nick').each(function() {
+            $(this).siblings(".review_title").insertAfter(this);
+        });
+    }
+    //personal_cabinet_menu
+    if ($(window).width() < 1040) {
+        $(".head_block_3 .user_link").insertBefore(".head_block_3 .my_panel");
+        $(".head_block_3 .sale_btn").insertAfter(".head_block_3 .my_panel");
+    }
+
+    $(window).resize(function() {
+        if ($(window).width() < 1040) {
+            $(".head_block_3 .user_link").insertBefore(".head_block_3 .my_panel");
+            $(".head_block_3 .sale_btn").insertAfter(".head_block_3 .my_panel");
+        }
+    });
+    //search
+    if ($(window).width() < 1040) {
+
+        $(".head_search_form .search__submit").insertAfter(".select_search select");
+    }
+
+    //mobile_icons
+    $('.icon_man').on('click', function() {
+        $('.head_block_3').slideToggle(300);
+    });
+    $('.icon_search').on('click', function() {
+        $('.search_block').slideToggle(300);
+    });
+    //tabs
+    (function($) {
+        $(function() {
+
+            $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+                $(this)
+                    .addClass('active').siblings().removeClass('active')
+                    .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+            });
+
+        });
+    })(jQuery);
+
+
+    //banner_mnu_block
+    $('.banner_mnu_block .mnu_list li').on('click', function() {
+        $(this)
+            .addClass('active').siblings().removeClass('active')
+    });
 
 
 
+
+
+
+    //Countdown Clock
+    function getTimeRemaining(endtime) {
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+        var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function initializeClock(id, endtime) {
+        var clock = document.getElementById(id);
+
+        var hoursSpan = clock.querySelector('.hours');
+        var minutesSpan = clock.querySelector('.minutes');
+        var secondsSpan = clock.querySelector('.seconds');
+
+        function updateClock() {
+            var t = getTimeRemaining(endtime);
+
+
+            hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+            minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+            secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+            if (t.total <= 0) {
+                clearInterval(timeinterval);
+            }
+        }
+
+        updateClock();
+        var timeinterval = setInterval(updateClock, 1000);
+    }
+
+    var deadline = new Date(Date.parse(new Date()) + 13 * 60 * 60 * 1000);
+    initializeClock('clockdiv1', deadline);
+    initializeClock('clockdiv2', deadline);
+    initializeClock('clockdiv3', deadline);
+    initializeClock('clockdiv4', deadline);
+    initializeClock('clockdiv5', deadline);
+    initializeClock('clockdiv6', deadline);
+
+
+
+
+
+
+
+
+    //fancybox
     $(".fancybox").fancybox();
 
 });
---
+++ js/simpleMobileMenu.js
@@ -0,0 +1,67 @@
+(function ( $ ) {
+ 
+    $.fn.smplmnu = function(options) {
+
+    	//settings
+    	var settings = $.extend({
+            // These are the defaults.
+            background: "#363D48",
+            speed: "0.5s"
+        }, options );
+
+
+    	//vars
+      	var hittrigger = $(this);
+    	var clspos = hittrigger.next('ul');
+    	var menutextcolor = hittrigger.next('ul li a');
+    	var overllay = '<div class="overally"></div>';
+
+    	//manipulating dom
+    	hittrigger.addClass('inrwrpr');
+    	clspos.addClass('inrwrpr');
+    	$('.inrwrpr').wrapAll( "<div class='navwrp'>" );
+    	   clspos.prepend('<a href="javascript:void(0)"><div class="menu_logo mnuclose" style="background: url(img/menu_logo.png) no-repeat 50%;"></div></a>');
+    	$('body').prepend(overllay);
+    	
+ 		
+ 		//functions and methods
+        hittrigger.click(function(event){
+			hittrigger.next('ul').addClass('mobimenu');
+		    $('.mobimenu').addClass('mnuopn');
+		    $('.mnuopn').css({
+		    	'z-index': '9999',
+		    	'background-color': settings.background,
+		    	'transition': settings.speed
+		    });
+		    $('.mnuopn a').css({
+		    	'color': settings.textColor
+		    });
+		    $('.overally').addClass('ovrActv');
+           // $('.main_logo').css({
+           //  'visibility': 'hidden'
+           //  });
+           $('.left_menu_button').addClass('open');
+            $(".main_logo").addClass('open');
+           
+		});
+
+        $('.mnuclose').click(function(event) {
+			hittrigger.next('ul').removeClass('mnuopn');
+			$('.overally').removeClass('ovrActv');
+            		});
+
+		$('.overally').click(function(event) {
+			clspos.removeClass('mnuopn');
+			$(this).removeClass('ovrActv');
+            // $('.main_logo, .left_menu_button').css({
+            // 'visibility': 'visible'
+            // });
+            $('.left_menu_button').removeClass('open');
+            $(".main_logo").removeClass('open');
+		});
+
+		return this				
+ 
+    };
+ 
+}( jQuery ));
